6a0e5e60cc028a79feb2a148ca466e12198d3f27
Lucas Almeida <lalmeida464@gmail.com>
2022-04-29 18:40:58 -0300

    very bad solution

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,15 +1,47 @@
 package aplication;
 
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Account;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
 		
 		
+		System.out.println();
+		System.out.println("Enter amount for withdraw:");
+		double amount = sc.nextDouble();
+		
+		if (amount > acc.getWithdrawLimit()) {
+			System.out.println("Withdraw error: The amount exceeds withdraw limit");
+		}
+		else if ( amount > acc.getBalance()){
+			System.out.println("Withdraw error: Not enough balance");
+		}
+		
+		else {
+			acc.withdraw(amount);
+			System.out.printf("New balance: %.2f%n", acc.getBalance());
+			
+		}
 		
 		sc.close();
 	}
